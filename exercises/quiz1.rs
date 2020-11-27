a81b9a0fbbaeec50870b8e5e8a91722a91d83dc5
RibhiEl-Zaru <ribhielzaru@gmail.com>
2020-11-27 16:25:47 -0500

    Finished quiz 1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,12 +7,15 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn ..... {
 
-// Don't modify this function!
+fn calculate_apple_price(apple_count : i32) -> i32 {
+    if apple_count > 40 { return apple_count }
+    else { return apple_count * 2}
+
+}
+
 #[test]
 fn verify_test() {
     let price1 = calculate_apple_price(35);
